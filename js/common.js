ee702bfdb2f7ed9093a8c181b5ed0eb5d4ff452a
apospelov13 <small99@hotbox.ru>
2017-07-01 13:35:59 +0300

    v1

--- js/common.js
+++ js/common.js
@@ -3,6 +3,13 @@ $(document).ready(function() {
 	$(".btnclick").click(function(){
 		$(".testclick").toggle("slow");
 	});		
+		function mediaSize() { 
+			if (window.matchMedia('(max-width: 767px)').matches) {
+				alert("privet");	
+			}
 
+	};
+  mediaSize();
+	window.addEventListener('resize', mediaSize, false);  
 });
 
